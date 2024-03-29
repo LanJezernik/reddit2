01ea5b2ba8cd34b15416bc0b8cc3991253f12950
aleks1234vre <shotbow.jekull@gmail.com>
2024-03-29 13:27:43 +0100

    user fix

---
+++ src/user/user.controller.ts
@@ -0,0 +1,21 @@
+import {Body, Controller, Delete, Get, Post} from '@nestjs/common';
+import {UserService} from "./user.service";
+import {CreateUserDto} from "./create-user.dto";
+
+// @ts-ignore
+// @ts-ignore
+@Controller('users')
+export class UserController {
+
+    constructor(private userService: UserService) {
+    }
+    @Get()
+    getAll() {
+        return this.userService.getAll();
+    }
+
+    @Post()
+    async createUser(@Body() createUserDto:CreateUserDto) {
+        return this.userService.createUser(createUserDto);
+    }
+}
